4ca772622cafdbc10ac13c3253f1418dae52a15e
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-07-12 14:45:55 +0300

    mobile SE

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,12 @@ function scrollFunction(){
         document.getElementById('btnUp').className = 'button-up hidden';
     }
 }
+
+// Modal window
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
